84e0b1205f4636cfbb07f0000408ebb0addd7b01
kewo22 <kewinf271@gmail.com>
2019-10-25 15:24:05 +0530

    Temp Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,38 +21,43 @@ export class AppComponent implements OnInit {
 
   data: any[];
 
-  grid: any = {
+  grid: Grid = {
     columnDefs: [
       {
-        name: "guid",
-        type: "string",
-        width: ""
-      },
-      {
-        name: "isActive",
-        type: "boolean"
-      },
-      {
-        name: "balance",
-        type: "string"
-      },
-      {
-        name: "phone",
-        type: "string"
-      },
-      {
-        name: "age",
-        type: "number"
-      },
-      {
-        name: "email",
-        type: "string"
+        label: "test"
       }
     ]
   };
 
   ngOnInit() {
     this.data = new Data().data;
+
+    const gridd: Grid = JSON.parse(JSON.stringify(this.grid));
+
+    for (let i = 0; i < Object.keys(this.data[0]).length; i++) {
+      if (gridd.columnDefs[i] && gridd.columnDefs[i].label) {
+        // User specified some
+        const x: ColumnDefinition = {
+          colType: typeof this.data[0][Object.keys(this.data[0])[i]],
+          width: "",
+          name: Object.keys(this.data[0])[i],
+          label: gridd.columnDefs[i].label
+        };
+        gridd.columnDefs.push(x);
+      } else {
+        // User not specified some
+        const x: ColumnDefinition = {
+          colType: typeof this.data[0][Object.keys(this.data[0])[i]],
+          width: "",
+          name: Object.keys(this.data[0])[i],
+          label: Object.keys(this.data[0])[i]
+        };
+        gridd.columnDefs.push(x);
+      }
+    }
+
+    this.grid = JSON.parse(JSON.stringify(gridd));
+
   }
 
   onScroll(w) {
@@ -62,4 +67,15 @@ export class AppComponent implements OnInit {
       "px";
     this.renderer.setStyle(this.panevScroll.nativeElement, "width", x);
   }
+}
+
+interface Grid {
+  columnDefs: ColumnDefinition[];
+}
+
+interface ColumnDefinition {
+  label: string;
+  name?: string;
+  width?: string;
+  colType?: string;
 }
